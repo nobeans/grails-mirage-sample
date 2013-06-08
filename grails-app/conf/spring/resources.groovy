7e958df07e825e7587ad5703aacaaa5a6bcf4ba2
Yasuharu NAKANO <ynak@jggug.org>
2013-06-08 23:45:18 +0900

    GrailsConnectionProviderではなく、既存のSpringConnectionProviderを使ってみたがエラーになるのでコメントを書いておいた
    SpringConnectionProviderは、Grailsが使っているDataSource実装のGrailsHibernateTransactionManagerを
    受け取れないため、独自のGrailsConnectionProviderが必要という流れ。

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -5,6 +5,18 @@ beans = {
         dataSource = ref('dataSource')
     }
 
+    // SpringConnectionProviderは、Grailsが使っているDataSource実装のGrailsHibernateTransactionManagerを
+    // 受け取れないため、独自のGrailsConnectionProviderが必要という流れ。
+    //
+    // java.lang.IllegalStateException: Cannot convert value of type
+    // [org.codehaus.groovy.grails.orm.hibernate.GrailsHibernateTransactionManager] to required type
+    // [org.springframework.jdbc.datasource.DataSourceTransactionManager] for property 'transactionManager':
+    // no matching editors or conversion strategy found
+    //
+    //connectionProvider(jp.sf.amateras.mirage.integration.spring.SpringConnectionProvider) {
+    //    transactionManager = ref('transactionManager')
+    //}
+
     sqlManager(jp.sf.amateras.mirage.SqlManagerImpl) {
         connectionProvider = connectionProvider
     }
