f692b14c577c9a0eea3706a124cde0ff182d77c8
Yasuharu NAKANO <ynak@jggug.org>
2013-06-08 23:26:30 +0900

    CustomerServiceをリファクタリングした
    - 変数名を変更
    - Txの振る舞いを探りたいのでassertをこのタイミングで仕込んでみた
    - など

--- grails-app/services/grails/mirage/sample/CustomerService.groovy
+++ grails-app/services/grails/mirage/sample/CustomerService.groovy
@@ -1,26 +1,26 @@
 package grails.mirage.sample
 
 import com.area_b.samples.Customer
-import jp.sf.amateras.mirage.SqlManager
 
 class CustomerService {
 
     def sqlManager
 
     def list() {
-        return sqlManager.getResultList(
-                Customer.class, "com/area_b/samples/select.sql");
+        sqlManager.getResultList(Customer.class, "com/area_b/samples/select.sql")
     }
 
     def update() {
-        int count = sqlManager.getCount("com/area_b/samples/select.sql")
+        int countBefore = sqlManager.getCount("com/area_b/samples/select.sql")
 
         sqlManager.executeUpdate("com/area_b/samples/insert.sql")
 
-        int count2 = sqlManager.getCount("com/area_b/samples/select.sql")
+        int countAfter = sqlManager.getCount("com/area_b/samples/select.sql")
+        int countAfterByGorm = Customer.count()
 
-        int count3 = Customer.count
+        println(countBefore + ":" + countAfter + ":" + countAfterByGorm)
 
-        print(count + ":" + count2 + ":" + count3)
+        assert countBefore + 1 == countAfter
+        assert countAfter == countAfterByGorm
     }
 }
