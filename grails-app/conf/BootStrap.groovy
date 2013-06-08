98c695f8d89e08c9654d3222e2bd4066f12d5d06
Yasuharu NAKANO <ynak@jggug.org>
2013-06-08 22:43:36 +0900

    BootStrapをリファクタリングした
    - failOnError: trueを使っておくと、予期しない問題があって保存できない場合に例外が発生してすぐに気がつける。
      デフォルトでは失敗しても自前でhasErros()とか戻り値チェックをしないと失敗してることに気づけない。
    - Groovyライクにリストを使ってみた
    - developmentのときだけBootStrapでダミーデータを追加するのが良いと思う。
      テストの時はテストの都合があるので、テストのsetupでフィクスチャを構築すればOK、というポリシ。

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,15 +3,15 @@ import com.area_b.samples.Customer
 class BootStrap {
 
     def init = { servletContext ->
-
-        Customer c = new Customer()
-        c.name = "Taro Yamada"
-        c.save()
-        Customer c2 = new Customer()
-        c2.name = "Jiro Yoshida"
-        c2.save()
-
+        environments {
+            development {
+                ["Taro Yamada", "Jiro Yoshida"].each { name ->
+                    new Customer(name: name).save(failOnError: true)
+                }
+            }
+        }
     }
+
     def destroy = {
     }
 }
