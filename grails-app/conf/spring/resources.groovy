bfc9cde31f1e12a099adcb8be6b30a9541815d83
Yasuharu NAKANO <ynak@jggug.org>
2013-06-08 13:47:05 +0900

    Springのbean定義を見直して、GrailsのデフォルトのdataSourceを使うようにした
    これによってGrailsとMirageで同一コネクション/同一トランザクションが使えるようになる。
    また、DataSource.groovyでの設定が効くので、非DRYなdialect指定などは不要になる。はず。

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,26 +1,15 @@
 // Place your Spring DSL code here
 beans = {
 
-    dataSource(org.apache.commons.dbcp.BasicDataSource) { bean ->
-        bean.destroyMethod = 'close'
-        driverClassName = '${dataSource.driverClassName}'
-        url = '${dataSource.url}'
-        username = '${dataSource.username}'
-        password = '${dataSource.password}'
-    }
-
     transactionManager(org.springframework.jdbc.datasource.DataSourceTransactionManager) {
-        dataSource = dataSource
+        dataSource = ref('dataSource')
     }
 
     connectionProvider(jp.sf.amateras.mirage.integration.spring.SpringConnectionProvider) {
         transactionManager = transactionManager
     }
 
-    dialect(jp.sf.amateras.mirage.dialect.MySQLDialect)
-
     sqlManager(jp.sf.amateras.mirage.SqlManagerImpl) {
         connectionProvider = connectionProvider
-        dialect = dialect
     }
 }
