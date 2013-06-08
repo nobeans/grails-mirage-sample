b117ad765dd3192ce8c6211631f93eeab3e490c9
Yasuharu NAKANO <ynak@jggug.org>
2013-06-08 13:48:29 +0900

    Springから取得したSqlManagerをそのまま使うようにした
    bean定義の見直しにより、sqlManagerをそのまま使えるようになったため。

--- grails-app/services/grails/mirage/sample/CustomerService.groovy
+++ grails-app/services/grails/mirage/sample/CustomerService.groovy
@@ -1,17 +1,11 @@
 package grails.mirage.sample
 
-import com.area_b.SqlManagerFactory
 import com.area_b.samples.Customer
 import jp.sf.amateras.mirage.SqlManager
-import jp.sf.amateras.mirage.dialect.MySQLDialect
 
 class CustomerService {
 
-    SqlManager sqlManager
-
-    def sessionFactory
-
-    MySQLDialect dialect
+    def sqlManager
 
     def list() {
         return sqlManager.getResultList(
@@ -19,14 +13,11 @@ class CustomerService {
     }
 
     def update() {
+        int count = sqlManager.getCount("com/area_b/samples/select.sql")
 
-        SqlManager manager = SqlManagerFactory.createInstance(sessionFactory, dialect)
+        sqlManager.executeUpdate("com/area_b/samples/insert.sql")
 
-        int count = manager.getCount("com/area_b/samples/select.sql")
-
-        manager.executeUpdate("com/area_b/samples/insert.sql")
-
-        int count2 = manager.getCount("com/area_b/samples/select.sql")
+        int count2 = sqlManager.getCount("com/area_b/samples/select.sql")
 
         int count3 = Customer.count
 
